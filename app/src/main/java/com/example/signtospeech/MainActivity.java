3d3bdc5a39da454962ab489f76714c6862433494
Gayatri Pawar <gayatripawar912@gmail.com>
2026-01-20 23:12:30 +0530

    Add stable finger counting using convexity defects

--- app/src/main/java/com/example/signtospeech/MainActivity.java
+++ app/src/main/java/com/example/signtospeech/MainActivity.java
@@ -7,19 +7,6 @@ import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-
-import org.opencv.imgproc.Imgproc;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,8 +16,21 @@ import androidx.core.content.ContextCompat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfInt4;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements CameraBridgeViewBase.CvCameraViewListener2 {
@@ -40,6 +40,11 @@ public class MainActivity extends AppCompatActivity
     JavaCameraView cameraView;
     TextView detectedText;
     Button btnSpeak;
+
+    // ===== STABILITY VARIABLES =====
+    int stableFingerCount = 0;
+    int frameCounter = 0;
+    int fingerSum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,13 +120,14 @@ public class MainActivity extends AppCompatActivity
             cameraView.disableView();
     }
 
-    // ================= CAMERA FRAMES =================
+    // ================= CAMERA CALLBACKS =================
     @Override
     public void onCameraViewStarted(int width, int height) { }
 
     @Override
     public void onCameraViewStopped() { }
 
+    // ================= CORE LOGIC =================
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
@@ -142,13 +148,8 @@ public class MainActivity extends AppCompatActivity
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
 
-        Imgproc.findContours(
-                mask,
-                contours,
-                hierarchy,
-                Imgproc.RETR_EXTERNAL,
-                Imgproc.CHAIN_APPROX_SIMPLE
-        );
+        Imgproc.findContours(mask, contours, hierarchy,
+                Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
         double maxArea = 0;
         MatOfPoint handContour = null;
@@ -162,19 +163,105 @@ public class MainActivity extends AppCompatActivity
         }
 
         if (handContour != null) {
-            Rect boundingRect = Imgproc.boundingRect(handContour);
 
-            Imgproc.rectangle(
-                    frame,
-                    boundingRect.tl(),
-                    boundingRect.br(),
-                    new Scalar(0, 255, 0),
-                    2
-            );
+            Rect boundingRect = Imgproc.boundingRect(handContour);
+            Imgproc.rectangle(frame, boundingRect.tl(),
+                    boundingRect.br(), new Scalar(0, 255, 0), 2);
+
+            MatOfInt hull = new MatOfInt();
+            Imgproc.convexHull(handContour, hull);
+
+            Point[] contourPoints = handContour.toArray();
+            int[] hullIndexes = hull.toArray();
+
+            List<Point> hullPoints = new ArrayList<>();
+            for (int index : hullIndexes) {
+                hullPoints.add(contourPoints[index]);
+            }
+
+            MatOfPoint hullMat = new MatOfPoint();
+            hullMat.fromList(hullPoints);
+
+            List<MatOfPoint> hullList = new ArrayList<>();
+            hullList.add(hullMat);
+
+            Imgproc.drawContours(frame, hullList, -1,
+                    new Scalar(255, 0, 0), 2);
+
+            MatOfInt4 defects = new MatOfInt4();
+            Imgproc.convexityDefects(handContour, hull, defects);
+
+            int[] defectArray = defects.toArray();
+            int fingerCount = 0;
+
+            for (int i = 0; i < defectArray.length; i += 4) {
+
+                int startIdx = defectArray[i];
+                int endIdx = defectArray[i + 1];
+                int farIdx = defectArray[i + 2];
+                double depth = defectArray[i + 3] / 256.0;
+
+                Point startPoint = contourPoints[startIdx];
+                Point endPoint = contourPoints[endIdx];
+                Point farPoint = contourPoints[farIdx];
+
+                double ang = angle(startPoint, endPoint, farPoint);
+
+                if (depth > 30 && ang < 90 &&
+                        farPoint.y < boundingRect.y + boundingRect.height * 0.8) {
+                    fingerCount++;
+                }
+            }
+
+            int totalFingers;
+
+             // Closed fist
+            if (fingerCount == 0) {
+                totalFingers = 0;
+            }
+             // One finger (special case)
+            else if (fingerCount == 1) {
+                totalFingers = 1;
+            }
+             // General case
+            else {
+                totalFingers = fingerCount;
+            }
+            // Safety cap (noise control)
+            if (totalFingers > 5) {
+                totalFingers = 5;
+            }
+
+
+
+            // == TEMPORAL SMOOTHING ==
+            fingerSum += totalFingers;
+            frameCounter++;
+
+            if (frameCounter >= 8) {
+                stableFingerCount = Math.round((float) fingerSum / frameCounter);
+                frameCounter = 0;
+                fingerSum = 0;
+            }
+
+            Imgproc.putText(frame,
+                    "Fingers: " + stableFingerCount,
+                    new Point(boundingRect.x, boundingRect.y - 10),
+                    Imgproc.FONT_HERSHEY_SIMPLEX,
+                    0.8,
+                    new Scalar(255, 255, 255),
+                    2);
         }
 
         return frame;
     }
 
+    // ===== ANGLE FUNCTION =====
+    private double angle(Point s, Point e, Point f) {
+        double a = Math.hypot(e.x - f.x, e.y - f.y);
+        double b = Math.hypot(s.x - f.x, s.y - f.y);
+        double c = Math.hypot(e.x - s.x, e.y - s.y);
 
+        return Math.acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+    }
 }
